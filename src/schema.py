cd167bcd3cb165bd19d9d8cc7489871e97f7550c
markshawn2020 <shawninjuly@gmail.com>
2024-03-28 22:23:15 +0800

    fixed NOT_GIVEN

--- src/schema.py
+++ src/schema.py
@@ -33,9 +33,9 @@ class CallLLMProviderBody(BaseModel):
     user: str | NotGiven = NOT_GIVEN,
     # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
     # The extra values given here take precedence over values defined on the client or passed to this method.
-    extra_headers: Headers | None = None,
-    extra_query: Query | None = None,
-    extra_body: Body | None = None,
+    extra_headers: Headers | None | NotGiven = NOT_GIVEN,
+    extra_query: Query | None | NotGiven = NOT_GIVEN,
+    extra_body: Body | None | NotGiven = NOT_GIVEN,
     timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
     
     class Config:
