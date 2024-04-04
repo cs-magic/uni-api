998455f5be71509ee2e946b5cea29fd291758c74
markshawn2020 <shawninjuly@gmail.com>
2024-04-04 18:01:21 +0800

    split driver part

--- src/wechat/uni_parser_bot.py
+++ src/wechat/uni_parser_bot.py
@@ -21,6 +21,52 @@ from packages.common_wechat.utils import parse_url_from_wechat_message
 from settings import settings
 
 
+def crawl_wechat_card(target_url: str, user_name: str = None, user_avatar: str = None):
+    try:
+        logger.debug("-- starting browser")
+        # todo: conditional
+        service = Service(ChromeDriverManager().install())
+        options = Options()
+        options.add_argument("--headless")
+        driver = webdriver.Chrome(service=service, options=options)
+        
+        logger.debug("-- visiting")
+        driver.get(f'{settings.FRONTEND_BASEURL}/card/gen')
+        
+        if user_name:
+            logger.debug("-- filling user name")
+            driver.find_element(By.ID, "card-user-name").send_keys(user_name)
+        
+        if user_avatar:
+            logger.debug("-- filling user avatar")
+            driver.find_element(By.ID, "card-user-avatar").send_keys(user_avatar)
+        
+        logger.debug("-- filling url")
+        driver.find_element(By.ID, "card-input-url").send_keys(target_url)
+        
+        logger.debug("-- clicking generate button")
+        driver.find_element(By.ID, "generate-card").click()
+        
+        logger.debug("-- waiting upload button")
+        WebDriverWait(driver, 30).until(
+            expected_conditions.element_to_be_clickable((By.ID, "upload-card"))
+        ).click()
+        
+        logger.debug("-- waiting uploaded result")
+        uploaded_tip = WebDriverWait(driver, 30).until(
+            expected_conditions.visibility_of_element_located((
+                By.CSS_SELECTOR, ".toaster div[data-title]"))
+        ).text
+        
+        oss_url = parse_first_url(uploaded_tip)
+        logger.info(f"-- oss url: {oss_url}")
+        return {"url": oss_url, "name": f"{get_current_timestamp(kind='s')}.png"}
+    
+    except Exception as e:
+        logger.error(e)
+        return None
+
+
 class UniParserBot(BaseWechatyBot):
     enabled = True
     
@@ -59,7 +105,6 @@ class UniParserBot(BaseWechatyBot):
         
         if room_name:
             if re.search(r'CS魔法社|test', room_name):
-                
                 if type == MessageType.MESSAGE_TYPE_URL:
                     url_model = parse_url_from_wechat_message(msg)
                     
@@ -70,48 +115,11 @@ class UniParserBot(BaseWechatyBot):
                     if (
                         url_model.type == "wxmp-article"  # todo: more types
                     ):
-                        logger.debug("-- starting browser")
-                        # todo: conditional
-                        service = Service(ChromeDriverManager().install())
-                        options = Options()
-                        options.add_argument("--headless")
-                        driver = webdriver.Chrome(service=service, options=options)
-                        
-                        logger.debug("-- visiting")
-                        driver.get(f'{settings.FRONTEND_BASEURL}/card/gen')
-                        
-                        if sender_name:
-                            logger.debug("-- filling user name")
-                            driver.find_element(By.ID, "card-user-name").send_keys(sender_name)
-                        
-                        if sender_avatar:
-                            logger.debug("-- filling user avatar")
-                            driver.find_element(By.ID, "card-user-avatar").send_keys(sender_avatar)
-                        
-                        logger.debug("-- filling url")
-                        driver.find_element(By.ID, "card-input-url").send_keys(url_model.url)
-                        
-                        logger.debug("-- clicking generate button")
-                        driver.find_element(By.ID, "generate-card").click()
-                        
-                        logger.debug("-- waiting upload button")
-                        WebDriverWait(driver, 30).until(
-                            expected_conditions.element_to_be_clickable((By.ID, "upload-card"))
-                        ).click()
-                        
-                        logger.debug("-- waiting uploaded result")
-                        uploaded_tip = WebDriverWait(driver, 30).until(
-                            expected_conditions.visibility_of_element_located((
-                                By.CSS_SELECTOR, ".toaster div[data-title]"))
-                        ).text
-                        oss_url = parse_first_url(uploaded_tip)
-                        if oss_url:
-                            file_name = f"{get_current_timestamp(kind='s')}.png"
-                            logger.info(f'sending Card(url={oss_url}, name={file_name})')
-                            filebox = FileBox.from_url(oss_url, file_name)
-                            await conversation.say(filebox)
-                            logger.info("sent")
-                        return
+                        data = crawl_wechat_card(url_model.url, sender_name, sender_avatar)
+                        if data:
+                            logger.info("-- sending")
+                            await conversation.say(FileBox.from_url(data["url"], data["name"]))
+                            logger.info("-- sent")
                 
                 if text == "DING":
                     await conversation.say("DONG")
