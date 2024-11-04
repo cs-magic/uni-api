5e8d7981751a82d943af82402e5beba916204875
markshawn2020 <shawninjuly@gmail.com>
2024-11-05 01:45:35 +0800

    better structure

--- src/router/uni_pusher/__init__.py
+++ src/router/uni_pusher/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from .jike import router as jike_router
 from .zsxq import router as zsxq_router
 
-uni_pusher_router = APIRouter(prefix='/uni-pusher')
+uni_pusher_router = APIRouter(prefix='/uni-pusher', tags=['uni-pusher'])
 
 uni_pusher_router.include_router(jike_router)
 uni_pusher_router.include_router(zsxq_router)
