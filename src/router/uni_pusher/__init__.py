dd3df5eb8ca8e945706f0c2581c92132d824da66
markshawn2020 <shawninjuly@gmail.com>
2024-11-07 13:53:01 +0800

    .

--- src/router/uni_pusher/__init__.py
+++ src/router/uni_pusher/__init__.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from .jike_post_content import router as jike_post_content_router
 from .jike_get_profile import router as jike_get_profile_router
 from .jike_search import router as jike_search_router
+from .jike_search_quanzi import router as jike_search_quanzi_router
 
 from .zsxq_post_content import router as zsxq_post_content_router
 from .zsxq_get_profile import router as zsxq_get_profile_router
@@ -12,6 +13,7 @@ uni_pusher_router = APIRouter(prefix='/uni-pusher', tags=['uni-pusher'])
 uni_pusher_router.include_router(jike_post_content_router)
 uni_pusher_router.include_router(jike_get_profile_router)
 uni_pusher_router.include_router(jike_search_router)
+uni_pusher_router.include_router(jike_search_quanzi_router)
 
 uni_pusher_router.include_router(zsxq_post_content_router)
 uni_pusher_router.include_router(zsxq_get_profile_router)
