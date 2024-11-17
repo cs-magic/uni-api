2174c0e7a6d6ae44c4f0a1cedb1970aea5301788
markshawn2020 <shawninjuly@gmail.com>
2024-11-17 23:01:02 +0800

    fixed database

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -43,7 +44,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = os.environ['DATABASE_BACKEND_URL'] # config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
